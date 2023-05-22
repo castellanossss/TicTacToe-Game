0c6789315767a469cbde8475752d549d57cfa747
Leonardo Castellanos <jaider.castellanos@uptc.edu.co>
2023-05-22 16:51:10 -0500

    Package Presenter necessary classes added

---
+++ src/presenter/Constants.java
@@ -0,0 +1,26 @@
+package presenter;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Constants {
+    public static final Color DRAW_BG = new Color(40, 124, 171);
+    public static final Color DRAW_BRD = new Color(9, 69, 158);
+    public static final Color LOSE_BG = new Color(161, 32, 32);
+    public static final Color LOSE_BRD = new Color(134, 5, 5);
+    public static final Color WIN_BG = new Color(30, 120, 22);
+    public static final Color WIN_BRD = new Color(7, 91, 6);
+    public static final ImageIcon TROPHY_ICON = new ImageIcon("icons/trophy.png");
+    public static final ImageIcon CPU_ICON = new ImageIcon("icons/ai.png");
+    public static final ImageIcon SERIOUS_ICON = new ImageIcon("icons/serious.png");
+    public static final String WIN_MESSAGE = "YOU WON!!!";
+    public static final String LOSE_MESSAGE = "CPU WON!!!";
+    public static final String DRAW_MESSAGE = "IT'S A DRAW!!!";
+    public static final String DEV_NAME = "Jaider Leonardo Castellanos Peña";
+    public static final String DEV_CODE = "COD - 202010436";
+    public static final String DEV_FACULTY = "Facultad de Ingeniería";
+    public static final String DEV_SCHOOL = "Escuela de Ingeniería de Sistemas y Computación";
+    public static final String DEV_COLLEGE_YEAR = "UPTC Tunja 2023 - 1";
+    public static final ImageIcon COLLEGE_ICON = new ImageIcon("icons/uptc.png");
+    public static final String PATH = "data/game_history.txt";
+}
